a66420a695f1ef6ce05fbe8332b5f6fcfa0b9f02
olgamie <olga.mierzwa@gmail.com>
2017-01-24 22:35:03 +0100

    get meetup comments

---
+++ R/get_meetup_comments.R
@@ -0,0 +1,16 @@
+#' Get the comments
+#'
+#' @param urlname The name of the group.
+#' @param api_key Your api key.
+#' @param event_id The id of the event.
+#' @export
+get_meetup_comments <- function(urlname, api_key, event_id){
+  api_url <- paste0(meetup_api_prefix(), urlname, "/events/", event_id, "/comments")
+  .fetch_results(api_url, api_key)
+}
+
+
+# Example:
+# urlname <- "rladies-san-francisco"
+# event_id <- past_events[[1]]$id
+# get_meetup_attendees(urlname, api_key, event_id)
