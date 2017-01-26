61dc590916da8c20191ff71e0f4eecb0e2980e24
olgamie <olga.mierzwa@gmail.com>
2017-01-26 19:07:36 +0100

    add get meetup boards

---
+++ R/get_boards.R
@@ -0,0 +1,18 @@
+#' Get the boards from a meetup group
+#'
+#' @param urlname The name of the group.
+#' @param api_key Your api key.
+#' @return List containing requested events.
+#'
+#'@examples
+#' \dontrun{
+#' urlname <- "rladies-nashville"
+#' api_key <- Sys.getenv("rladies_api_key")
+#' meetup_boards <- get_boards(urlname = urlname,
+#'                       api_key = api_key)
+#'}
+#' @export
+get_boards <- function(urlname, api_key) {
+  api_url <- paste0(meetup_api_prefix, urlname, "/boards")
+  .fetch_results(api_url, api_key)
+}
