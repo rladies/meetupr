85ebf34451d3e951d413ab4e05b121dd96cdde32
ledell <erin@h2o.ai>
2017-11-05 18:51:27 -0800

    Renaming get_attendees() to get_event_attendees() for emphasis/clarity

--- R/get_event_attendees.R
+++ R/get_event_attendees.R
@@ -1,7 +1,7 @@
 #' Get the attendees for a specified event
 #'
 #' @template urlname
-#' @param event_id Character. The id of the event. Event id's can be obtained
+#' @param event_id Character. The id of the event. Event ids can be obtained
 #'   using [get_events()].
 #' @template api_key
 #'
@@ -10,16 +10,18 @@
 #'    * name
 #'    * status
 #'    * resource
+#' @references
+#' \url{https://www.meetup.com/meetup_api/docs/:urlname/events/:id/attendance/#list}
 #' @examples
 #' \dontrun{
 #' urlname <- "rladies-nashville"
 #' past_events <- get_events(urlname = urlname,
 #'                       event_status = "past")
-#' event_id <- past_events$id[1]
-#' attendees <- get_attendees(urlname, event_id)
+#' event_id <- past_events$id[1]  #first event for this group
+#' attendees <- get_event_attendees(urlname, event_id)
 #'}
 #' @export
-get_attendees <- function(urlname, event_id, api_key = NULL){
+get_event_attendees <- function(urlname, event_id, api_key = NULL) {
   api_params <- paste0(urlname,
                        "/events/",
                        event_id,
@@ -28,7 +30,7 @@ get_attendees <- function(urlname, event_id, api_key = NULL){
   tibble::tibble(
     id = purrr::map_chr(res, c("member", "id")),
     name = purrr::map_chr(res, c("member", "name")),
-    status = purrr::map_chr(res, "status"),
+    status = purrr::map_chr(res, "status"),  #currently always "attended" so this is not very useful
     resource = res
   )
 }
