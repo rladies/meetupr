94eda19258b893180e699b23e63d67d97331518f
ledell <erin@h2o.ai>
2017-11-06 00:10:24 -0800

    Adding new fields to get_boards() tibble

--- R/get_boards.R
+++ R/get_boards.R
@@ -1,13 +1,21 @@
-#' Get the boards from a meetup group
+#' Get the discussion boards from a meetup group
 #'
 #' @template urlname
 #' @template api_key
 #' @return A tibble with the following columns:
 #'    * id
 #'    * name
+#'    * description
+#'    * created
+#'    * updated
 #'    * post_count
+#'    * discussion_count
+#'    * latest_reply_created
+#'    * latest_reply_member_name
 #'    * resource
 #'
+#' @references
+#' \url{https://www.meetup.com/meetup_api/docs/:urlname/boards/}
 #'@examples
 #' \dontrun{
 #' urlname <- "rladies-nashville"
@@ -19,9 +27,15 @@ get_boards <- function(urlname, api_key) {
   api_method <- paste0(urlname, "/boards")
   res <- .fetch_results(api_method, api_key)
   tibble::tibble(
-    id = purrr::map_chr(res, "id"),
+    id = purrr::map_int(res, "id"),
     name = purrr::map_chr(res, "name"),
-    post_count = purrr::map_int(res, "post_count"),
+    description = purrr::map_chr(res, "description"),
+    created = .date_helper(purrr::map_dbl(res, "created")),
+    updated = .date_helper(purrr::map_dbl(res, "updated")),
+    post_count = purrr::map_int(res, "post_count", .null = NA),
+    discussion_count = purrr::map_int(res, "discussion_count", .null = NA),
+    latest_reply_created = .date_helper(purrr::map_dbl(res, c("latest_reply", "created"), .null = NA)),
+    latest_reply_member_name = purrr::map_chr(res, c("latest_reply", "member", "name"), .null = NA),
     resource = res
   )
 }
