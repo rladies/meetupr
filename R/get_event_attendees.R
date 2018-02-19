1ea013c7f0de57df676421442cb6ecc8f73fef49
Perry Stephenson <perry.stephenson@gmail.com>
2018-02-19 22:27:11 +1100

    Allow additional API params

--- R/get_event_attendees.R
+++ R/get_event_attendees.R
@@ -4,6 +4,7 @@
 #' @param event_id Character. The id of the event. Event ids can be obtained
 #'   using [get_events()] or by looking at the event page URL.
 #' @template api_key
+#' @param ... Other options passed through to API
 #'
 #' @return A tibble with the following columns:
 #'    * id
@@ -21,12 +22,12 @@
 #' attendees <- get_event_attendees(urlname, event_id)
 #'}
 #' @export
-get_event_attendees <- function(urlname, event_id, api_key = NULL) {
+get_event_attendees <- function(urlname, event_id, api_key = NULL, ...) {
   api_method <- paste0(urlname,
                     "/events/",
                     event_id,
                     "/attendance")
-  res <- .fetch_results(api_method, api_key)
+  res <- .fetch_results(api_method, api_key, ...)
   tibble::tibble(
     id = purrr::map_int(res, c("member", "id")),
     name = purrr::map_chr(res, c("member", "name")),
