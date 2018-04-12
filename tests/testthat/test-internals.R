bbfc85c8a8f416ca7dd7a55f7d290557a14091eb
Augustina Ragwitz <augustina.ragwitz@ibm.com>
2018-04-11 21:30:56 -0700

    Fix get_events() unit test and add case to find_groups()  * get_events now tests the get_events call  * added TODO's for future tests based on logical branches  * find_groups now runs with and without optional params
    Part of Issue #31

--- tests/testthat/test-internals.R
+++ tests/testthat/test-internals.R
@@ -14,3 +14,7 @@ test_that(".quick_fetch() success case", {
   expect_equal(names(res), c("result", "headers"), info="check .quick_fetch() return value")
   expect_equal(res$headers$`content-type`, "application/json;charset=utf-8", info="check .quick_fetch() header content-type")
 })
+
+# TODO .fetch_results()
+
+# TODO .fetch_results() no api key
