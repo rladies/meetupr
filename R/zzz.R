4429a2fb3e06af33f7d10c0171cdc83bb3e8dc51
Greg Sutcliffe <github@emeraldreverie.org>
2019-08-16 08:16:56 +0100

    Add default secret
    Co-Authored-By: Erin LeDell <erin@h2o.ai>

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
   op.meetupr <- list(
     meetupr.httr_oauth_cache = TRUE,
     meetupr.consumer_key     = "2vagj0ut3btomqbb32tca763m1",
-    meetupr.consumer_secret  = "blah", # and this
+    meetupr.consumer_secret  = "k73s3jrah57hp9ej21e8dslnl5",
     meetupr.use_oauth        = TRUE
   )
   toset <- !(names(op.meetupr) %in% names(op))
