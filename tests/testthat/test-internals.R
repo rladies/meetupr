62fe38781c0b09b5692e85673465fedc876f6df9
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-01-08 15:29:15 +0100

    add test for when the results are empty

--- tests/testthat/test-internals.R
+++ tests/testthat/test-internals.R
@@ -10,6 +10,20 @@ test_that(".quick_fetch() success case", {
   expect_equal(res$headers$`content-type`, "application/json; charset=utf-8")
 })
 
+test_that(".quick_fetch() empty case", {
+  api_method <- "rladies-nashville/events"
+  vcr::skip_if_vcr_off()
+  vcr::use_cassette("quick_fetch_empty", {
+    res <- testthat::expect_warning(.quick_fetch(
+      api_method = api_method,
+      event_status = "upcoming"
+    ))
+  })
+  expect_equal(names(res), c("result", "headers"))
+  expect_equal(res$headers$`content-type`, "application/json; charset=utf-8")
+})
+
+
 # TODO .fetch_results()
 
 # TODO .fetch_results() no api key
