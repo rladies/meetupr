89a0e401ea21db6bffebd6fa0c552ef83acf8115
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-02-07 21:13:41 +0100

    propose fix to #82

---
+++ R/meetup_query.R
@@ -0,0 +1,26 @@
+
+#' General Meetup query
+#'
+#' Create your own query to the Meetup API
+#' (https://www.meetup.com/meetup_api/docs/).
+#'
+#'
+#' @param api_path url path to send the meetup query after the 'https://api.meetup.com/'
+#' @param ... additional parameters to the query
+#'
+#' @return raw list results from the query, untitied
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' meetup_query("rladies-oslo")
+#'
+#' meetup_query("rladies-oslo", event_statue = "past")
+#'
+#' }
+meetup_query <- function(api_path,
+                         ...){
+  .fetch_results(api_path,
+               ...)
+}
