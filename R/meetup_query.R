e2577da6a1356e6520e7baaa529e9de81a8e41db
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-02-08 08:49:51 +0100

    add comment about usefulness to meetup_quary

--- R/meetup_query.R
+++ R/meetup_query.R
@@ -1,14 +1,18 @@
-
 #' General Meetup query
 #'
 #' Create your own query to the Meetup API
 #' (https://www.meetup.com/meetup_api/docs/).
-#'
+#' This function is convenient if you want to create
+#' a query to other Meetup API endpoints that we
+#' have not created functions for. If you do create
+#' a function that quieries a currently unsupported
+#' endpoint and a results tidier for this, we are happy
+#' to receive PR's with your suggestion.
 #'
 #' @param api_path url path to send the meetup query after the 'https://api.meetup.com/'
 #' @param ... additional parameters to the query
 #'
-#' @return raw list results from the query, untitied
+#' @return raw list results from the query, not tidied
 #' @export
 #'
 #' @examples
