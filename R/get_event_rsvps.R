a456c4efc560870ba2102be507a43a9135ffa3ad
benubah <ben@rpowerlabs.org>
2022-04-12 23:23:12 +0100

    update get_event_rsvps docs + tests

--- R/get_event_rsvps.R
+++ R/get_event_rsvps.R
@@ -45,6 +45,23 @@ get_event_rsvps <- function(urlname, event_id,
 
 #' @param id Required event ID
 #' @param ... Should be empty. Used for parameter expansion
+#' @return A tibble with the following columns:
+#'    * member_id
+#'    * member_name
+#'    * member_url
+#'    * member_is_host
+#'    * guests
+#'    * response
+#'    * event_id
+#'    * event_title
+#'    * event_url
+#'    * created
+#'    * updated
+#' @references
+#' \url{https://www.meetup.com/api/schema/#Event}
+#' \url{https://www.meetup.com/api/schema/#Ticket}
+#' \url{https://www.meetup.com/api/schema/#User}
+#' @examples
 #' \dontrun{
 #' rsvps <- get_event_rsvps2(id = "103349942!chp")
 #' }
@@ -63,7 +80,24 @@ get_event_rsvps2 <- function(
     .token = token
   )
 
-  dt
+  dt %>%
+    dplyr::rename(
+      member_id = .data$user.id,
+      member_name = .data$user.name,
+      member_url = .data$user.memberUrl,
+      event_id = .data$event.id,
+      event_title = .data$event.title,
+      event_url = .data$event.eventUrl,
+      member_is_host = .data$isHost,
+      guests = .data$guestsCount,
+      response = .data$status,
+      created = .data$createdAt,
+      updated = .data$updatedAt
+    ) %>%
+    dplyr::mutate(
+      created = anytime::anytime(.data$created),
+      updated = anytime::anytime(.data$updated)
+    )
 
 }
 
---
+++ tests/testthat/test-get_event_rsvps.R
@@ -0,0 +1,15 @@
+expected_names <- c("member_id", "member_name", "member_url", "member_is_host",
+                    "response", "guests", "event_id", "event_title",
+                    "event_url", "created", "updated")
+
+test_that("get_event_rsvps() works with one status", {
+  vcr::use_cassette("get_event_rspvs", {
+    rsvps <-  get_event_rsvps2(id = "103349942")
+  })
+  expect_s3_class(rsvps, "data.frame")
+
+  expect_true(
+    all(
+      names(rsvps) == expected_names
+    ))
+})
