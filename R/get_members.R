80d261f14e30354f6bb92c530b6f5130df948ecc
benubah <ben@rpowerlabs.org>
2022-04-13 01:05:18 +0100

    update get_members docs + tests

--- R/get_members.R
+++ R/get_members.R
@@ -47,6 +47,19 @@ get_members <- function(urlname,
 
 #' @param urlname Required urlname of the Meetup group
 #' @param ... Should be empty. Used for parameter expansion
+#' @return A tibble with the following columns:
+#'    * id
+#'    * name
+#'    * member_url
+#'    * photo_link
+#'    * status
+#'    * role
+#'    * created
+#'    * most_recent_visit
+#' @references
+#' \url{https://www.meetup.com/api/schema/#GroupMembership}
+#' \url{https://www.meetup.com/api/schema/#User}
+#' @examples
 #' \dontrun{
 #' members <- get_members2("rladies-remote")
 #' }
@@ -65,7 +78,20 @@ get_members2 <- function(
     .token = token
   )
 
-  dt
-
+  dt %>%
+    dplyr::rename(
+      id = .data$node.id,
+      name = .data$node.name,
+      member_url = .data$node.memberUrl,
+      photo_link = .data$node.memberPhoto.baseUrl,
+      status = .data$metadata.status,
+      role = .data$metadata.role,
+      created = .data$metadata.joinedDate,
+      most_recent_visit = .data$metadata.mostRecentVisitDate
+    ) %>%
+    dplyr::mutate(
+      created = anytime::anytime(.data$created),
+      most_recent_visit = anytime::anytime(.data$most_recent_visit)
+    )
 }
 
