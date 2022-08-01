ca08e6ddf859341cbd97dc3e990f570cdcda986b
benubah <ben@rpowerlabs.org>
2022-08-01 02:25:42 +0100

    pro groups and events: R <-> gql communication

--- R/graphql.R
+++ R/graphql.R
@@ -390,6 +390,54 @@ gql_get_event_comments <- graphql_query_generator(
   pb_format = "- :current/?? :elapsed :spin"
 )
 
+
+gql_get_pro_groups <- graphql_query_generator(
+  "find_pro_groups",
+  cursor_fn = function(x) {
+    pageInfo <- x$data$proNetworkByUrlname$groupsSearch$pageInfo
+    if (pageInfo$hasNextPage) {
+      list(cursor = pageInfo$endCursor)
+    } else {
+      NULL
+    }
+  },
+  total_fn = function(x) {
+    x$data$proNetworkByUrlname$groupsSearch$count
+    Inf
+  },
+  extract_fn = function(x) {
+    groups <- lapply(x$data$proNetworkByUrlname$groupsSearch$edges, function(item) {
+      item$node
+    })
+    groups
+  },
+  pb_format = "- :current/?? :elapsed :spin"
+)
+
+gql_get_pro_events <- graphql_query_generator(
+  "find_pro_events",
+  cursor_fn = function(x) {
+    pageInfo <- x$data$proNetworkByUrlname$eventsSearch$pageInfo
+    if (pageInfo$hasNextPage) {
+      list(cursor = pageInfo$endCursor)
+    } else {
+      NULL
+    }
+  },
+  total_fn = function(x) {
+    x$data$proNetworkByUrlname$eventsSearch$count
+    Inf
+  },
+  extract_fn = function(x) {
+    events <- lapply(x$data$proNetworkByUrlname$eventsSearch$edges, function(item) {
+      item$node
+    })
+    events
+  },
+  pb_format = "- :current/?? :elapsed :spin"
+)
+
+
 # Cache the country code to name conversion as the conversion is consistent
 country_code_mem <- local({
   cache <- list()
